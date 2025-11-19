ab048667214fd148a7805c2291d54bd670753571
Orestis <liaskos.orestis@gmail.com>
2025-11-19 17:25:49 +0200

    Added homework2 and instructions

---
+++ Assets/Homeworks/Homework2.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+// Add running animation to the existing vampire controller.
+// Create a folder in Assets called StudentWork (it will be ignored by git).
+// Place copies of whatever you want in that folder.
+// You can also create anything you want in that folder.
+public class Homework2 : MonoBehaviour
+{
+
+}
